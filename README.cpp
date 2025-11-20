4b68348999968c0e01a218c2f4950420e60cfa2f
hungvo231-coding <hungvo11032005@gmail.com>
2025-11-19 18:36:17 -0800

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -15,5 +15,11 @@
 //     Include fstream for writing and reading files
 //     Include string to use string type
 //     Include std to make print easier
-//     There are two items of data whose names and quantity, name stored in string type and quantity store in int type.
-//     saveToFile
+//     There are two items of data whose names and quantity, name stored in string type and quantity stored in int type.
+//     saveToFile() method is used to create an output file stream, check if it opens correctly or not, before inserting the data in a format name, quantity, before closing the file.
+//     loadToFile() method use to open the file through ifstream, then check if it opens correctly or not. Then read each line by using getline to print the content to the screen before closing the file.
+//     main() Function:
+//     Create an Item object Item myItem
+//     Set its name and quantity by myItem.name = _____ and myItem.quantity = ______ in this case it is "Wrench" and 5
+//     myItem.saveToFile("item.txt"); save item to the txt file name item.txt (This will create a file containing: Wrench,5)
+//     myItem.loadFromFile("item.txt"; this will load and print the file content: File content: Wrench,5.
